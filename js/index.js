7f17884f5ea698943475dcc2a3d0f56e0837e3a5
Anand Kumar <georgian3125anand@gmail.com>
2025-01-21 12:20:00 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Form Details:
+Name: ${name}
+Email: ${email}
+Subject: ${subject}
+Message: ${message}`);
+});
